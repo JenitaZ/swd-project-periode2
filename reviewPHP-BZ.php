409d4163e21e939590ff51994fbca5040ced68d8
ghostrosely <6047656@mborijnland.nl>
2024-12-06 16:47:31 +0100

    renamed file

---
+++ reviewPHP-BZ.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="description" content="Gameshop Website">
+    <meta name="keywords" content="Review games">
+    <meta name="author" content="Bingyi Zhou">
+    <link rel="stylesheet" href="swd-project-periode2/css/index.css">   <!--stylesheet-->
+</head>
+
+<body class="Home">
+    <!-- navigatie -->
+    <header class="headerTemplate">
+        <article class="logo gameslog">
+            <a href="index.html"><img src="img/gameshoplogo-transparant~000.png" alt="Logo Gameshop" class="logo-main"></a>
+            <h1 class="Name">GameStars</a></h1>
+
+            <section class="zoekbalk">
+                <input type="text" placeholder="search">
+                <div>
+                    <button type="submit" class="searchbtn"><i class="fa fa-search"></i></button>
+                </div>
+
+                <!-- light/dark mode toggle -->
+                <div class="toggle">
+                    <input type="checkbox" id="mode-toggle" class="toggle__input">
+                    <label for="mode-toggle" class="toggle__label"></label>
+                </div>
+
+                <i class="fa fa-user-circle login"></i>
+            </section>
+        </article>
+
+        <nav class="stick">
+        </nav>
+
+
+
+    </header>
+    <nav class="navbar">
+        <a href="index.html">Home</a>
+        <a href="games.html">Games</a>
+        <a href="Merchandise.html">Merchandise</a>
+        <a href="swd-project-periode2/contact.html">Contact</a>
+    </nav>
+
+    <main id="phpcontainer">
+        <section class="flex-contatiner">
+            <article class="reviewContainer1">
+                <img src="" alt="Image of the game" id="picture">
+            </article>
+
+        </div>
+        <main>
+    
+                <section class="containerReview">
+                    <div class="reviewContent">
+                        <h1 id="tItle"></h1>
+                        <p id="release"></p>
+                        <p id="Accessplatforms"></p>
+                        <p id="Description"></p>
+                    </div>
+                </section>
+            </section>
+    
+            <section class="ratingContainer"></section>
+        </main>
+    
+    
+    <footer>
+        <div class="footer-content">
+            <section class="footer-left">
+                <p class="footer-left">
+                <h1 class="footer-header">Contact</h1>
+                For any questions you can send us email directly through our <a href="contact.html"
+                    class="footer-header2">contact page!</a>
+                </p>
+            </section>
+    
+                <section class="footer-right">
+                    <p>
+                        <span class="footer-header">Follow us</span>
+                    <div class="divider"></div>
+    
+                <section class="icons">
+                    <a href="" class="iconlinks"><img src="img\socialmedia\yt-logo.png" alt="Youtube link"></a>
+                    <a href="" class="iconlinks"><img src="img\socialmedia\ig-logo.png" alt="Instagram link"></a>
+                    <a href="" class="iconlinks"><img src="img\socialmedia\X-logo.png" alt="X(Twitter) link"></a>
+                    <a href="" class="iconlinks"><img src="img\socialmedia\tk-logo.png" alt="Tiktok link"
+                            class="annoyance"></a>
+                    </section>
+                    </p>
+                </section>
+            </div>
+        </footer>
+<?php 
