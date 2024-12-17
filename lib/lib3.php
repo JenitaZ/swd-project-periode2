c4fefb1570da251c858f314cf0cd5a93b9a4a0f4
JenitaZ <eatwaternow@gmail.com>
2024-12-17 14:09:03 +0100

    yt links and genres

--- lib/lib3.php
+++ lib/lib3.php
@@ -31,11 +31,9 @@ $games = [
         
         "PEGI" => 12,
          "gameDesc" => "<span id='title'>Stray</span> <br> <span id='smallGrayText'>released on: July 19th, 2022, Developed by:  BlueTwelve Studio and released by Annapurna Interactive </span> <br><br> Is is a third-person cat adventure game set amidst the detailed, neon-lit alleys of a decaying cybercity and the murky environments of its seedy underbelly and follows a stray cat who falls into a walled city populated by robots, machines, and mutant bacteria, and sets out to return to the surface with the help of a drone companion, B-12",
-        // "genre" => "cats",
-        // "exploration",
-        // "adventure",
-        // "multiplayer",
-        "yt" => ""
+        "genre" => "sandbox", "survival", "2d",
+        "yt" => "https://www.youtube.com/embed/w7uOhFTrrq0?si=K9ud0bxzsfHG3W4M",
+        "rating" => 8.1
     ],
     "HK" => [
         "title" => "Hollowkight",
@@ -51,9 +49,9 @@ $games = [
 
         "PEGI" => 12,
         "gameDesc" => "<span id='title'>Banana</span> <br> <span id='smallGrayText'>released on: April 23rd, 2024, Developed by:  BlueTwelve Studio and released by Annapurna Interactive </span> <br><br>The core mechanic of Banana Clicker is as simple as it gets. Clicking on the banana increases your banana count. There's no strategy or complex controls involved, just pure, clicky fun<br><br>While clicking the Banana is the main activity, the real excitement comes from the random banana drops. These special bananas, featuring unique designs from cherry blossoms to gentlemen attire, are added to your Steam inventory.<br><br>The fun doesn't stop with collecting unique banana. These special bananas can be sold on the Steam Marketplace, opening the door to potentially lucrative (and strange) digital banana trading. Rarity plays a major role here, with some bananas fetching hundreds of dollars.",
-        // "genre" => "casual",
-        // "singleplayer",
-        "yt" => ""
+        "genre" => "difficult", "platformer",
+        "yt" => "https://www.youtube.com/embed/UAO2urG23S4?si=h57Cb664WEaKcCiR" ,
+        "rating" => 9
     ]
 ];
 
