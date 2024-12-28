d6f4f6f1d9f449680223d348cbae8ee0b95a5ec3
JenitaZ <eatwaternow@gmail.com>
2024-12-28 21:20:48 +0100

    deleted some duplicate code

--- reviewPHP2-JZ.php
+++ reviewPHP2-JZ.php
@@ -15,7 +15,7 @@ include "lib/PHP/reviews.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <meta name="description" content="Review 2 & 3 | PHP">
+    <meta name="description" content="Review 2 , 3, 4 | PHP">
     <meta name="keywords" content="stray, banana review PHP">
     <meta name="author" content="Jenita Zheng">
 
